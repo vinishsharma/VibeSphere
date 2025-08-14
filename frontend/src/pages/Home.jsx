28f37219df073a6b4315278c5fdea7d264d6e991
Vinish Sharma <vinishsharma@Vinishs-MacBook-Air.local>
2025-08-15 01:28:08 +0530

    new start

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div className='w-full sm:w-[90%] md:w-[80%] lg:w-[70%] min-h-screen mx-auto py-10 '>
-        <h1>Home</h1>
+      <div className='w-full sm:w-[90%] md:w-[80%] lg:w-[70%] min-h-screen mx-auto py-10 bg-black'>
+        <div>Home Page</div>
       </div>
     </>
   )
--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,8 +1,8 @@
 //Format the date before displaying
-const changeDateFormat = (isoDate) => {
-  if (!isoDate) return "Invalid Date";
+const changeDateFormat = (isDate) => {
+  if (!isDate) return "Invalid Date";
 
-  const date = new Date(isoDate);
+  const date = new Date(isDate);
   const options = { day: "numeric", month: "short", year: "numeric" };
 
   return date.toLocaleDateString("en-GB", options); // Example: "1 Jan 2000"
