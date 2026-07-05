a0967a6e16a963ca397f7ead602e8bd38abf0258
Vinish Sharma <vinishsharma@Vinishs-MacBook-Air.local>
2026-07-06 03:17:53 +0530

    notification bug fixed

--- backend/services/notification.service.js
+++ backend/services/notification.service.js
@@ -2,21 +2,11 @@ import Notification from "../models/notification.model.js";
 
 export const createNotification = async ({ receiverId, senderId, type, postId, message }) => {
   try {
+
     // Don't notify yourself
-    // if (receiverId.toString() === senderId.toString()) {
-    //   return;
-    // }
-
-    // // Prevent duplicate follow notifications
-    // if (type === "#follow") {
-    //   const alreadyExists = await Notification.findOne({
-    //     receiverId,
-    //     senderId,
-    //     type,
-    //   });
-
-    //   if (alreadyExists) return;
-    // }
+    if (receiverId.toString() === senderId.toString()) {
+      return;
+    }
 
     await Notification.create({
       receiverId,
